5d4543159bdccd62c1b29aef0406d43c0ef57fef
Naydelin <ncumezx@miumg.edu.gt>
2026-05-18 22:12:35 -0600

    avance metodo newton

--- metodos/urls.py
+++ metodos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('newton/', views.newton, name='newton'),
 ]
--- metodos/views.py
+++ metodos/views.py
@@ -1,4 +1,44 @@
 from django.shortcuts import render
 
+from sympy import symbols
+from sympy import sympify
+from sympy import diff
+
 def inicio(request):
     return render(request, 'inicio.html')
+
+def newton(request):
+
+    datos = {}
+
+    if request.method == 'POST':
+
+        funcion = request.POST['funcion']
+        x0 = request.POST['x0']
+        tolerancia = request.POST['tolerancia']
+        iteraciones = request.POST['iteraciones']
+
+        x = symbols('x')
+
+        f = sympify(funcion)
+        derivada = diff(f, x)
+
+        resultado = round(float(f.subs({x: float(x0)})), 4)
+        resultado_derivada = round(float(derivada.subs({x: float(x0)})), 4)
+
+        nuevo_x = round(
+        float(x0) - (resultado / resultado_derivada),
+        4
+        )
+
+        datos = {
+    'funcion': funcion,
+    'x0': x0,
+    'tolerancia': tolerancia,
+    'iteraciones': iteraciones,
+    'resultado': resultado,
+    'derivada': derivada,
+    'nuevo_x': nuevo_x
+}
+
+    return render(request, 'newton.html', datos)
