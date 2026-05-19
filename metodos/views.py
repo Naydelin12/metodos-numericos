29467d5cb42197bcc601071190fcdad27ffbea61
Naydelin <ncumezx@miumg.edu.gt>
2026-05-19 00:49:42 -0600

    newton raphson completo

--- metodos/views.py
+++ metodos/views.py
@@ -18,27 +18,83 @@ def newton(request):
         tolerancia = request.POST['tolerancia']
         iteraciones = request.POST['iteraciones']
 
+        tabla = []
+
         x = symbols('x')
 
         f = sympify(funcion)
+
         derivada = diff(f, x)
 
         resultado = round(float(f.subs({x: float(x0)})), 4)
-        resultado_derivada = round(float(derivada.subs({x: float(x0)})), 4)
 
-        nuevo_x = round(
-        float(x0) - (resultado / resultado_derivada),
-        4
+        resultado_derivada = round(
+            float(derivada.subs({x: float(x0)})),
+            4
         )
 
+        nuevo_x = round(
+            float(x0) - (resultado / resultado_derivada),
+            4
+        )
+
+        error = round(abs(nuevo_x - float(x0)), 4)
+
+        tabla.append([
+            1,
+            nuevo_x,
+            resultado,
+            resultado_derivada,
+            error,
+            'Se evaluó la función y se aplicó Newton-Raphson'
+        ])
+
+        contador = 1
+
+        while error > float(tolerancia) and contador < int(iteraciones):
+
+            x0 = nuevo_x
+
+            resultado = round(
+                float(f.subs({x: float(x0)})),
+                4
+            )
+
+            resultado_derivada = round(
+                float(derivada.subs({x: float(x0)})),
+                4
+            )
+
+            nuevo_x = round(
+                float(x0) - (resultado / resultado_derivada),
+                4
+            )
+
+            error = round(abs(nuevo_x - float(x0)), 4)
+
+            contador += 1
+
+            tabla.append([
+                contador,
+                nuevo_x,
+                resultado,
+                resultado_derivada,
+                error,
+                'Nueva aproximación calculada'
+            ])
+            
+
         datos = {
-    'funcion': funcion,
-    'x0': x0,
-    'tolerancia': tolerancia,
-    'iteraciones': iteraciones,
-    'resultado': resultado,
-    'derivada': derivada,
-    'nuevo_x': nuevo_x
-}
+            'funcion': funcion,
+            'x0': x0,
+            'tolerancia': tolerancia,
+            'iteraciones': iteraciones,
+            'resultado': resultado,
+            'derivada': derivada,
+            'nuevo_x': nuevo_x,
+            'tabla': tabla,
+            'raiz': nuevo_x,
+            'mensaje': 'El método convergió correctamente'
+        }
 
     return render(request, 'newton.html', datos)
